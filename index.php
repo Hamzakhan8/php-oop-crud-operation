2b1067ea7cbfcf9dc9c5fd715762060b5d1aa382
footersaib <hamxa564927@gmail.com>
2022-02-18 23:50:25 -0800

    update

--- index.php
+++ index.php
@@ -65,6 +65,47 @@
       </div>
     </div>
 </div>
+
+ <!--add user Modal -->
+<div class="modal " id="addModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">add new user</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body px-4">
+        <form action="" method="POST" id="form-data">
+          <div class="form-group">
+            <input type="text" name="fname" class="form-control" placeholder="first name">
+          </div>
+          <div class="form-group">
+            <input type="text" name="lname" class="form-control" placeholder="last name">
+          </div>
+          <div class="form-group">
+            <input type="text" name="email" class="form-control" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <input type="text" name="phone" class="form-control" placeholder="Phone">
+          </div>
+         
+        </form>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+       <input type="submit" name="insert" id="insert" value="add new user" class="btn btn-danger btn-block">
+      </div>
+
+    </div>
+  </div>
+</div>
+
+ <!-- edit user Modal  -->
 <div class="modal " id="addModal">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -107,7 +148,7 @@
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.11.4/datatables.min.js"></script>
@@ -153,8 +194,9 @@
                     data:$("#form-data").serialize()+"&action=insert",
                     success:function(response){
                          Swal.fire({
-                           title:'user added successfully',
-                           type:'success'
+                          title: "successfully added!",
+                          text: "You clicked the button!",
+                          icon: "success",
                          })
 
                          $("#addModal").modal('hide');
